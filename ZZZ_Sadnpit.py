075a7692d92c09c65f69bea3267dccadaf5b8759
DaleIce <olleti24938@masseyhigh.school.nz>
2025-02-11 11:00:54 +1300

    sandpit _ZZZZZ

---
+++ ZZZ_Sadnpit.py
@@ -0,0 +1,15 @@
+# reminder - here is an integer that has been multiplied by 3
+number = 6
+
+print(number * 4)
+
+#here is a string thath has been multiplied by 4
+# ( note thath speech marks and text color)
+num_string = "6"
+print(num_string * 4)
+
+#an example showing what the len (foo) method does
+example_text = "heloo eorld"
+text_length = len(example_text)
+
+print(f" '{example_text}' is {text_length} characters long")
