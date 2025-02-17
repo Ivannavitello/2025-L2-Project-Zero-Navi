71df56e3cc1fc3fa8e2901125a5c98b23281e08f
DaleIce <olleti24938@masseyhigh.school.nz>
2025-02-18 09:56:08 +1300

    Update C_05_number_checker_V1.py

--- C_05_number_checker_V1.py
+++ C_05_number_checker_V1.py
@@ -1,20 +1,19 @@
-# Functions go here
 def num_check(question, num_type, exit_code=None):
     """Checks users enter an integer / float that is more than
-     zero (or the optional exit code)"""
+    zero (or the optional exit code)"""
 
-        if num_type == "integer":
-            error = "Opps - please enter an integer more than zero."
-            change_to = int
-        else:
-            error = "Oops - please enter a number more than zero."
-            change_to = float
+    if num_type == "integer":
+        error = "Opps - please enter an integer more than zero."
+        change_to = int
+    else:
+        error = "Oops - please enter a number more than zero."
+        change_to = float
     while True:
         response = input(question).lower()
 
         # check for the exit code
         if response == exit_code:
-            return  response
+            return response
         try:
             # Change the response to an integer and check that it's more than zero
             response = change_to(response)
@@ -36,7 +35,12 @@ def num_check(question, num_type, exit_code=None):
 while True:
     print()
 
-    my_float = number_check("please enter a number more than 0:", "float")
+    my_float = num_check("please enter a number more than 0: ", "float")
+    print(f"Thanks. You chose {my_float}")
     print()
-    my_int = number_check("Please enter an integer more than 0",
+    my_int = num_check("Please enter an integer more than 0",
                           "integer", "")
+    if my_int == "":
+        print("You have chosen infinite mode.")
+    else:
+        print(f"Thanks. YOu chose {my_int}")
